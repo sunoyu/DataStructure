e87f827b41a6432065c0ec673ac7749dcb9c6a65
sunoyu <suno_yu@naver.com>
2024-07-13 19:19:10 +0900

    ArrayList 완성

--- src/main/java/ArrayList.java
+++ src/main/java/ArrayList.java
@@ -1,11 +1,8 @@
-package Interface_form.List;
-
 import Interface_form.List;
 
-import java.security.PublicKey;
 import java.util.Arrays;
 
-public class ArrayList<E> implements List<E>
+public class ArrayList<E> implements List<E>, Cloneable
 {
     // 생성자
     private static final int DEFAULT_CAPACITY = 10;  // least capacity size
@@ -147,30 +144,88 @@ public class ArrayList<E> implements List<E>
     }
     @Override
     public E remove(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        E element = (E) array[index]; // 삭제될 요소를 반환하기 위해 임시로 깔아둠
+        array[index] = null;
+
+        // 삭제된 요소에 의해 뒤에있는 모든 요소를 앞으로 한칸씩
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+            array[i + 1] = null;
+        }
+        size--;
+        resize();
+        return element;
     }
 
     @Override
     public boolean remove(Object value) {
-        return false;
+        // 삭제하고자 하는 요소의 인덱스 찾기
+        int index = indexOf(value);
+
+        // -1이라면 array에 요소가 없다는 의미이므로 false 반환
+        if (index == -1) {
+            return false;
+        }
+
+        // index 위치에 있는 요소를 삭제
+        remove(index);
+        return true;
     }
 
 
 
     @Override
     public int size() {
-        return 0;
+        return size; // 요소 갯수 반환
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0; // 요소가 0개일 경우 비어있다는 의미
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
+        resize();
     }
 
-    private
+
+    // 부록 메소드
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+
+        // 새로운 객체 생성
+        ArrayList<?> cloneList = (ArrayList<?>)super.clone();
+
+        // 새로운 객체의 배열도 생성해주어야 함 (객체는 얕은복사가 되기 때문)
+        cloneList.array = new Object[size];
+
+        // 배열의 값을 복사함
+        System.arraycopy(array, 0, cloneList.array, 0, size);
+
+        return cloneList;
+    }
+
+    public Object[] toArray() {
+        return Arrays.copyOf(array, size);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size) {
+            // copyOf(원본 배열, 복사할 길이, Class<? extends T[]> 타입)
+            return (T[]) Arrays.copyOf(array, size, a.getClass());
+        }
+        // 원본배열, 원본배열 시작위치, 복사할 배열, 복사할배열 시작위치, 복사할 요소 수
+        System.arraycopy(array, 0, a, 0, size);
+        return a;
+    }
+
 }
